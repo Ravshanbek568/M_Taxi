4f5f93e3047148f5fd22c9b5a5c87838f490878c
Ravshanbek568 <aolimxon29@gmail.com>
2025-07-18 16:30:43 +0500

    M_Taxi2

---
+++ M_Taxi2/android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+import com.android.build.gradle.internal.dsl.DefaultConfig
+import org.gradle.api.JavaVersion
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.example.m_taksi"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.m_taksi"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("androidx.multidex:multidex:2.0.1")
+}
