e47dcaf116046aecdf8a373dc2dbf168b0311579
Ravshanbek568 <aolimxon29@gmail.com>
2025-07-18 15:45:58 +0500

    M_taxi

--- M_taxi/android/app/build.gradle.kts
+++ M_taxi/android/app/build.gradle.kts
@@ -1,15 +1,15 @@
+import com.android.build.gradle.internal.dsl.DefaultConfig
+import org.gradle.api.JavaVersion
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")  // Firebase plugin qo'shildi
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
+    compileSdk = 34
     namespace = "com.example.m_taksi"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -17,55 +17,32 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.m_taksi"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-        multiDexEnabled = true  // Agar Firebase Auth yoki Firestore ishlatilsa
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             signingConfig = signingConfigs.getByName("debug")
-             // Proguard qoidalari (agar kerak bo'lsa)
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
 }
 
-flutter {
-    source = "../.."
-}
-
-
 dependencies {
-    // Flutterning asosiy dependencysi
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
-    
-    // Firebase BoM (Bill of Materials)
-    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
-    
-    // Firebase modullari (kerakli bo'lganlarini qo'shing)
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")     // Authentication uchun
-    implementation("com.google.firebase:firebase-firestore-ktx") // Firestore uchun
-    implementation("com.google.firebase:firebase-messaging-ktx") // FCM uchun
-    implementation("com.google.firebase:firebase-storage-ktx")  // Storage uchun
-    
-    // MultiDex (agar minSdk 21 dan past bo'lsa)
     implementation("androidx.multidex:multidex:2.0.1")
-    
-    // Flutter Firebase pluginsi uchun (pubspec.yaml da qo'shilgan bo'lsa)
-    implementation("com.google.firebase:firebase-core:21.1.1")
 }
--- M_taxi/android/build.gradle.kts
+++ M_taxi/android/build.gradle.kts
@@ -1,41 +1,18 @@
-// <project>/build.gradle.kts
-plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false  // Firebase plugin
-}
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.3")  // Qo'shimcha ravishda
-        
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.gms:google-services:4.4.3")
     }
 }
-
 
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
